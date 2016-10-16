267a3085434fcf579c792a6c0ec1aad3511c46e6
prasoongoyal <prasoongoyal13@gmail.com>
2016-10-16 09:56:24 -0400

    Completed fixed paths code.

--- gibbs_sampling_fixed_paths.py
+++ gibbs_sampling_fixed_paths.py
@@ -28,7 +28,7 @@ def initialize_parameters(W, gamma, alpha, vocab_size):
   params['alpha'] = alpha
   params['vocab_size'] = vocab_size
   params['num_levels'] = 4
-  params['branching_factor'] = 5
+  params['branching_factor'] = 2
   params['logbeta'] = log(vocab_size * gamma_fn(alpha)) - \
       log_gamma_fn(vocab_size * alpha)
   params['newpathprob'] = - vocab_size * (alpha - 1) * log(vocab_size) - \
@@ -36,8 +36,10 @@ def initialize_parameters(W, gamma, alpha, vocab_size):
   (num_docs, num_words) = np.shape(W)
   params['num_docs'] = num_docs
   params['num_words'] = num_words
-  params['max_iter'] = 1000
+  params['max_iter'] = 10000
+  params['iteration'] = -1
   params['temperature'] = 10.0
+  params['decay_factor'] = 0.999
   return params
 
 # Gets a flat array of counts in each cluster. Returns the probability of each
@@ -101,7 +103,9 @@ def sample_Z(W_d, Z_d, beta, n, counts, params):
   posterior_sum = sum(posterior) * 1.0
   posterior = map(lambda x:x/posterior_sum, posterior)
   Z_dn = multinomial(1, make_distr_peaked(posterior, \
-      params['temperature'])).tolist().index(1)
+      params['temperature']))
+  #print Z_dn
+  Z_dn = Z_dn.tolist().index(1)
   return Z_dn
 
 def computeCounts(Z_d, num_levels, branching_factor):
@@ -118,15 +122,67 @@ def computeCounts(Z_d, num_levels, branching_factor):
 
 def make_distr_peaked(values, temperature):
   #print temperature
-  new_values = map(lambda x:x**(1.0 / temperature), values)
-  if 1.0 / temperature > 500:
+  if 1.0 / temperature > 100:
     # numerical issues with direct calcuations
     new_values = map(lambda x: 1.0 if (x == max(values)) else 0.0, values)
+  else:
+    new_values = map(lambda x:x**(1.0 / temperature), values)
   norm = sum(new_values)
   new_values = map(lambda x:x/norm, new_values)
   #print values, new_values
   #raw_input()
   return new_values
+
+def filter_Z(W, Z):
+  try:
+    idx = W.tolist().index(-1)
+    return Z[:idx]
+  except ValueError:
+    return Z
+
+def CRP_likelihood(freq, gamma):
+  log_prob = 0.0
+  freq = filter(lambda x:x>0, freq)
+  for f in freq:
+    log_prob += log(factorial(freq[f] - 1))
+  log_prob += (len(freq) - 1) * log(gamma)
+  for k in range(1, sum(freq)):
+    log_prob -= log(k + gamma)
+  return log_prob
+
+def NCRP_likelihood(Z, params):
+  branching_factor = params['branching_factor']
+  num_levels = params['num_levels']
+  num_nodes = (branching_factor ** num_levels - 1) / (branching_factor - 1)
+  num_paths = branching_factor ** (num_levels - 1)
+  num_internal_nodes = num_nodes - num_paths
+  log_prob = 0.0
+  counts = [0] * num_paths
+  while len(counts) > 1:
+    prev_level_counts = []
+    while len(counts) > 0:
+      curr_counts = counts[:branching_factor]
+      counts = counts[branching_factor:]
+      log_prob += CRP_likelihood(curr_counts, params['gamma'])
+      prev_level_counts.append(sum(curr_counts))
+    counts = prev_level_counts
+  return log_prob
+
+# computes log likelihood
+def compute_likelihood(W, Z, beta, params):
+  likelihood = 0.0
+  alpha = params['alpha']
+  for curr_beta in beta:
+    for v in range(len(curr_beta)):
+      likelihood += (alpha - 1) * log(curr_beta[v])
+  for d in range(len(Z)):
+    likelihood += NCRP_likelihood(filter_Z(W[d], Z[d]), params)
+  for d in range(len(W)):
+    for n in range(len(W[0])):
+      if (W[d][n] == -1):
+        continue
+      likelihood += log(beta[Z[d][n]][int(W[d][n])])
+  return likelihood
 
 def run_gibbs_sampler(W, Z, beta, params):
   branching_factor = params['branching_factor']
@@ -134,11 +190,17 @@ def run_gibbs_sampler(W, Z, beta, params):
   num_nodes = (branching_factor ** num_levels - 1) / (branching_factor - 1)
   num_paths = branching_factor ** (num_levels - 1)
   num_internal_nodes = num_nodes - num_paths
+  best_Z = Z
+  best_beta = beta
+  best_likelihood = compute_likelihood(W, Z, beta, params)
   for iteration in range(params['max_iter']):
+    params['iteration'] += 1
+    params['temperature'] *= params['decay_factor']
     # sample Z
     for d in range(len(Z)):
       # compute counts for this document
-      counts = computeCounts(Z[d], params['num_levels'], params['branching_factor'])
+      counts = computeCounts(Z[d], params['num_levels'], \
+          params['branching_factor'])
       #print Z[d], counts
       #raw_input()
       for n in range(len(Z[0])):
@@ -149,9 +211,18 @@ def run_gibbs_sampler(W, Z, beta, params):
       all_docs_counts.append([params['alpha']] * params['vocab_size'])
     for d in range(len(Z)):
       for n in range(len(Z[0])):
+        #print 'Zdn', Z[d][n]
+        #print 'Wdn', W[d][n]
         all_docs_counts[Z[d][n]][W[d][n]] += 1
     for path_idx in range(len(beta)):
       beta[path_idx] = dirichlet(all_docs_counts[path_idx])
+    curr_likelihood = compute_likelihood(W, Z, beta, params)
+    if curr_likelihood > best_likelihood:
+      best_Z = Z
+      best_beta = beta
+      best_likelihood = curr_likelihood
+    print iteration, best_likelihood
+  return best_Z, best_beta
 
 def read_data(filename):
   with open(filename) as f:
@@ -202,9 +273,21 @@ def main():
   #print Z
   #print all_nodes_count
 
-  best_Z, best_beta = run_gibbs_sampler(W, Z, beta, params)
+  best_beta = beta
+  best_Z = Z
+  best_likelihood = compute_likelihood(W, Z, beta, params)
+  for trial in range(25):
+    new_Z, new_beta = run_gibbs_sampler(W, Z, beta, params)
+    new_likelihood = compute_likelihood(W, new_Z, new_beta, params)
+    if new_likelihood > best_likelihood:
+      best_beta = new_beta
+      best_Z = new_Z
+      best_likelihood = new_likelihood
   print 'best_Z', best_Z
-  print 'best_beta', best_beta
+  print 'best_beta'
+  for i, beta in enumerate(best_beta):
+    print i, beta
 
 if __name__=="__main__":
+  np.set_printoptions(suppress=True, precision=4)
   main()
